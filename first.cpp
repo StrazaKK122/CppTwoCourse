5629d8b2c699f4ecbcfffa9dc17ad58216906506
Mateusz Kuniczuk <matiusz98@gmail.com>
2019-10-13 17:20:14 +0200

    Initial commit

---
+++ first.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <cmath>
+#include <chrono>
+
+double exponent_gain(double gain, int years) {
+    if (years > 1) {
+        return (1 + gain) * exponent_gain(gain, years - 1);
+    } else return (1 + gain);
+}
+
+double compound_interest(double assets, double gain, int years) {
+    double result = assets * exponent_gain(gain, years);
+    std::cout << "The result is: " << result << std::endl;
+    return result;
+}
+
+
+void print_asterixes(int n) {
+    if (n >= 1) {
+        std::cout << "*" << std::endl;
+        print_asterixes(n - 1);
+    }
+}
+
+void print_numbers_down(int n) {
+    std::cout << n << " ";
+    if (n > 1) {
+        print_numbers_down(n - 1);
+    }
+}
+
+int euklides(int a, int b) {
+    if (b != 0) {
+        return euklides(b, a % b);
+    }
+
+    std::cout << "NWD tych liczb to: " << a << std::endl;
+    return a;
+}
+
+int sum_digits(int n) {
+    if (n >= 1) {
+        return n % 10 + sum_digits(n / 10);
+    }
+    return 0;
+}
+
+void print_sum_digits(int n) {
+    std::cout << sum_digits(n) << std::endl;
+}
+
+long int fib_recursion(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    } else {
+        return fib_recursion(n - 1) + fib_recursion(n - 2);
+    }
+}
+
+long int fib_iteration(int n) {
+    int prev_fib, last_fib = 0;
+    int current_fib = 1;
+    for (int i = 1; i < n; i++) {
+        prev_fib = last_fib;
+        last_fib = current_fib;
+        current_fib = prev_fib + last_fib;
+    }
+
+    return current_fib;
+}
+
+double fib_math(int n) {
+    return (pow(1 + sqrt(5), n) - pow(1 - sqrt(5), n)) / (pow(2, n) * sqrt(5));
+}
+
+void fib_wrapper(int n) {
+    // TODO: count all the times and compare them.
+    auto start_rec = std::chrono::high_resolution_clock::now();
+    std::cout << "Wynik fib_recursion: " << fib_recursion(n) << std::endl;
+    auto stop_rec = std::chrono::high_resolution_clock::now();
+    auto duration_rec = std::chrono::duration_cast<std::chrono::nanoseconds>(stop_rec - start_rec);
+    std::cout << "Rekurencja - czas wykonywania: " << duration_rec.count() << " nanosekund.\n";
+
+    auto start_iter = std::chrono::high_resolution_clock::now();
+    std::cout << "Wynik fib_iteration: " << fib_iteration(n) << std::endl;
+    auto stop_iter = std::chrono::high_resolution_clock::now();
+    auto duration_iter = std::chrono::duration_cast<std::chrono::nanoseconds>(stop_iter - start_iter);
+    std::cout << "Iteracja - czas wykonywania: " << duration_iter.count() << " nanosekund.\n";
+
+    auto start_math = std::chrono::high_resolution_clock::now();
+    std::cout << "Wynik fib_math: " << fib_math(n) << std::endl;
+    auto stop_math = std::chrono::high_resolution_clock::now();
+    auto duration_math = std::chrono::duration_cast<std::chrono::nanoseconds>(stop_math - start_math);
+    std::cout << "Stosunek matematyczny - czas wykonywania: " << duration_math.count() << " nanosekund.\n";
+
+
+}
+
+// MAIN FUNCTION
+int first() {
+
+    // compound_interest(10000, 0.03, 10);
+    // print_asterixes(5);
+    // print_numbers_down(5);
+    // euklides(36, 21);
+    // print_sum_digits(55);
+    // fib_wrapper(10);
+
+
+    // Don't touch that!
+    return 0;
+}
---
+++ second.cpp
@@ -0,0 +1,318 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+static int CURRENT_YEAR = 2019;
+
+class Segment {
+
+    float x1, y1;
+    float x2, y2;
+
+public:
+
+    void set_segment(float first_x, float first_y, float second_x, float second_y) {
+        x1 = first_x;
+        y1 = first_y;
+        x2 = second_x;
+        y2 = second_y;
+    }
+
+    double get_length() {
+        return (sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2)));
+    }
+
+    void flip(char axis) {
+        if (axis == 'X') {
+            y1 = -y1;
+            y2 = -y2;
+        } else if (axis == 'Y') {
+            x1 = -x1;
+            x2 = -x2;
+        } else {
+            cout << "Such axis doesn't exist. Try 'X' or 'Y'.\n";
+        }
+    }
+
+    void shift(float vector_x, float vector_y) {
+        x1 = x1 + vector_x;
+        y1 = y1 + vector_y;
+        x2 = x2 + vector_x;
+        y2 = y2 + vector_y;
+    }
+
+    void show() {
+        cout << "First point coordinates: (" << x1 << "," << y1 << ")\n";
+        cout << "Second point coordinates: (" << x2 << "," << y2 << ")\n";
+        cout << "Segment length: " << get_length() << endl;
+    }
+};
+
+class Complex {
+
+public:
+
+    float real_part = 0;
+    float irrational_part = 0;
+
+    void add(Complex add_complex) {
+
+        real_part = real_part + add_complex.real_part;
+        irrational_part = irrational_part + add_complex.irrational_part;
+    }
+
+    void subtract(Complex subtract_complex) {
+
+        real_part = real_part - subtract_complex.real_part;
+        irrational_part = irrational_part - subtract_complex.irrational_part;
+    }
+
+    void show() {
+        cout << "Number: " << real_part << " + " << irrational_part << "i.\n";
+    }
+
+};
+
+class Citizen {
+
+public:
+
+    string name;
+    string surname;
+    int day;
+    int month;
+    int year;
+
+    void set(string new_name, string new_surname, int new_day, int new_month, int new_year) {
+        name = new_name;
+        surname = new_surname;
+        day = new_day;
+        month = new_month;
+        year = new_year;
+    }
+
+    int get_age() {
+        return CURRENT_YEAR - year;
+    }
+
+    bool is_younger(Citizen another_citizen) {
+        if (year > another_citizen.year) {
+            return true;
+        } else if (year == another_citizen.year) {
+            if (month > another_citizen.month) {
+                return true;
+            } else if (month == another_citizen.month) {
+                return day > another_citizen.day;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+};
+
+class Book {
+    bool hard_cover;
+    int pages;
+    float price;
+    string genre;
+public:
+    string getGenre() {
+        return genre;
+    }
+
+    bool isHardCover() const {
+        return hard_cover;
+    }
+
+    int getPages() const {
+        return pages;
+    }
+
+    float getPrice() const {
+        return price;
+    }
+
+    void setHardCover(bool hardCover) {
+        hard_cover = hardCover;
+    }
+
+    void setPages(int pages) {
+        pages = pages;
+    }
+
+    void setPrice(float price) {
+        price = price;
+    }
+
+    void setGenre(string genre) {
+        genre = genre;
+    }
+
+};
+
+class Coffee {
+    bool has_milk;
+    int sugar_teaspoons;
+    bool is_iced;
+    float price;
+public:
+    bool isHasMilk() const {
+        return has_milk;
+    }
+
+    void setHasMilk(bool hasMilk) {
+        has_milk = hasMilk;
+    }
+
+    int getSugarTeaspoons() const {
+        return sugar_teaspoons;
+    }
+
+    void setSugarTeaspoons(int sugarTeaspoons) {
+        sugar_teaspoons = sugarTeaspoons;
+    }
+
+    bool isIced() const {
+        return is_iced;
+    }
+
+    void setIsIced(bool isIced) {
+        is_iced = isIced;
+    }
+
+    float getPrice() const {
+        return price;
+    }
+
+    void setPrice(float price) {
+        price = price;
+    }
+
+};
+
+class Person {
+    string surname;
+    int age;
+    int status = 0;
+
+public:
+
+    void dopisz(string new_surname, int new_age) {
+        cout << "Dodaje " << new_surname << ", " << new_age << "... OK.\n";
+        surname = new_surname;
+        age = new_age;
+        status = 1;
+    }
+
+    int zmien(string new_surname, int new_age) {
+        if (status == 0) {
+            return 1;
+        }
+        cout << "Zmieniam " << surname << " --> " << new_surname << ", "<< age << " --> " << new_age << "... OK.\n";
+        surname = new_surname;
+        age = new_age;
+        return 0;
+    }
+    int zmien(int new_age, string new_surname) {
+        if (status == 0) {
+            return 1;
+        }
+        cout << "Zmieniam " << surname << " --> " << new_surname << ", "<< age << " --> " << new_age << "... OK.\n";
+        surname = new_surname;
+        age = new_age;
+        return 0;
+    }
+    int zmien(string new_surname) {
+        if (status == 0) {
+            return 1;
+        }
+        cout << "Zmieniam " << surname << " --> " << new_surname  << "... OK.\n";
+        surname = new_surname;
+        return 0;
+    }
+
+    int zmien(int new_age) {
+        if (status == 0) {
+            return 1;
+        }
+        cout << "Zmieniam " << age << " --> " << new_age << "... OK.\n";
+        age = new_age;
+        return 0;
+    }
+
+    int drukuj() {
+        if (status == 0) {
+            return 1;
+        }
+        cout << surname << ", " << age << endl;
+        return 0;
+    }
+
+    int skasuj() {
+        if (status == 0) {
+            return 1;
+        }
+        cout << "Kasuje: " << surname << ", " << age << "...\n";
+        surname = "";
+        age = -1;
+        status = 0;
+        return 0;
+    }
+};
+
+// MAIN FUNCTION
+int main() {
+
+    /*
+    Segment segment{};
+    segment.set_segment(1, 3, 5, 15);
+    segment.show();
+    segment.flip('Y');
+    segment.show();
+    segment.shift(10, 10);
+    segment.show();
+    */
+
+    /*
+    Complex number;
+    number.real_part = 10;
+    number.irrational_part = 5;
+    number.show();
+    number.add(number);
+    number.show();
+     */
+
+    /*
+    Citizen citizen;
+    citizen.set("Andrzej", "Kowalski", 10, 8, 1998);
+    Citizen another_citizen;
+    another_citizen.set("Gustaw", "Zabek", 23, 4, 1998);
+    if (citizen.is_younger(another_citizen)) {
+        cout << citizen.name << " jest mlodszy od " << another_citizen.name << endl;
+    } else {
+        cout << citizen.name << " jest starszy od " << another_citizen.name << endl;
+    }
+    */
+
+    /*
+    Book book;
+    book.setGenre("fantasy");
+    cout << book.getGenre();
+    */
+
+    /*
+    Person test;
+    test.dopisz("janusz", 21);
+    test.zmien("jaroslaw");
+    test.drukuj();
+    cout << test.skasuj();
+    test.skasuj();
+    cout << test.skasuj();
+    */
+
+
+    return 0;
+}
